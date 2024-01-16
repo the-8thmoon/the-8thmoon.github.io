1819416f1398305c9271fd51272afc0b5b71e2bd
Alan <alan.williams.brito@gmail.com>
2024-01-16 06:48:57 -0300

    Restore

--- src/portfolio/portfolio.jsx
+++ src/portfolio/portfolio.jsx
@@ -8,19 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
         <Profile />
-        <Profile />
-        <Profile />
-        <a href="#tab1">Tab 1</a>
-<a href="#tab2">Tab 2</a>
-
-<a id="tab1"></a>
-<a id="tab2"></a>
-
-<div class="tab tab1 default-tab">Some content</div>
-<div class="tab tab2">Some other content</div>
-        <Profile />
-        <Profile />
-        <Profile />
 
     </React.StrictMode>,
 )
