76f379c79cfc7668ae4e2c68eaa2c71e50729096
Alan <alan.williams.brito@gmail.com>
2024-01-16 02:35:28 -0300

    Test changes

--- src/ProfileBio.jsx
+++ src/ProfileBio.jsx
@@ -3,7 +3,7 @@
 function ProfileBio(){
     return(
         <div className="profile-bio">
-            <h1>Alan Brito - Moon Follower</h1>
+            <h1>Alan Brito<br></br>Moon Follower</h1>
                 {/* <p>
                      Hello! My name is Alan, I'm a gamedev enthusiast, trying to do some art along the way.
                      My main focus is gameplay programmer, I've been using Unity for about <b>5 years</b>. 
@@ -15,7 +15,7 @@ function ProfileBio(){
                 <p>
                      Hello! My name is Alan, I'm a C# developer with vast experience in Game Dev (Unity). <br></br>
                      Currently exploring Web Development via <b>ASP.NET</b> and <b>React.js</b>.<br></br> 
-                     I consider myself an experimentation expert. I love to delve into diverse areas, just out of curiosity or to find a way to solve a self-imposed problem.
+                     I like to experiment with different kind of projects, eithr by curiosity or by solving self-imposed problems
                 </p>
         </div>
     )
