0ba75978ecb6066f5bc6478576d1519b20132cc1
Alan <alan.williams.brito@gmail.com>
2024-09-12 18:25:08 -0300

    Updated Readme

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,13 +1,11 @@
 import Elements from "./Elements"
 import Profile from "./Profile"
-import ProfileNav from "./ProfileNav"
 
 
 function Body() {
     return (
         <div className="body-page">
             <Profile />
-            {/* <ProfileNav/> */}
             <hr></hr>
             <Elements />
         </div>
